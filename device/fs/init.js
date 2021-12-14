fc9f3ff331f85b38a50fa4fcebb83986b3a769a7
kfirle <49530231+kfirle@users.noreply.github.com>
2021-12-15 00:45:22 +0200

    Working on ports 21....23

--- device/fs/init.js
+++ device/fs/init.js
@@ -6,55 +6,86 @@ load('api_mqtt.js');
 let config_topic = '/devices/' + Cfg.get('device.id') + '/config';
 let state_topic = '/devices/' + Cfg.get('device.id') + '/state';
 
-let leds = {
-	r:{pin:23, state:0}, g:{pin:22, state:0},b:{pin:21, state:0}
+let state = {
+	pins: []
 };
 
-let dev_id = Cfg.get('device.id');
-let led_keys = ["r", "g", "b"];
+for(let i=0; i<30; ++i) {
+	state.pins[i] = 0;
+}
 
-print("Started with DeviceID: " + dev_id);
+
+print("Started with DeviceID: " + Cfg.get('device.id'));
 print("Listening for configuration on: " + config_topic + " topic");
 
-for(let i=0; i<led_keys.length; ++i) {  
-	let led = led_keys[i];
-	print("Setting the mode of pin ", leds[led].pin, " (", led, ") to MODE_OUTPUT");
-	GPIO.set_mode(leds[led].pin, GPIO.MODE_OUTPUT);
+for(let i=21; i<=23; ++i) {  
+	print("Setting pin ",i, "to MODE_OUTPUT");
+	GPIO.set_mode(i, GPIO.MODE_OUTPUT);
+	state.pins[i] = 0;
+	GPIO.write(i, 0);
 }
+
 
 MQTT.sub(config_topic, function(conn, topic, msg) {
 	
 	print("Received a new configuration");
 
-	 let obj = JSON.parse(msg);
-	  
-	  if(obj) {
-		  for(let i=0; i<led_keys.length; ++i) {  
-			let led = led_keys[i];
+	let obj = JSON.parse(msg) || {};
+	
+	print("Configuraiton: ", obj);
+	
+	if(!obj.pins) {
+		print('"pins" property is missing fron configuration JSON');
+		return;
+	}
+	
+	let new_state = {
+		pins: []
+	};
+	
+	// update existing pins
+	for(let i=0; i<obj.pins.length; ++i) {
+		
+		// new state
+		let pin = obj.pins[i];
+		let found = 0;
+		
+		print("Updating pin", pin.number, "from", state.pins[pin.number], "to", pin.value);
+		state.pins[pin.number] = pin.value;
+		GPIO.write(pin.number, pin.value);
+		
+		new_state.pins.push({
+			number: pin.number,
+			value: pin.value
+		});
+		
+	}
 
-			leds[led].state = obj[led];
-
-			if(leds[led].state) {				
-				print("Setting pin ", leds[led].pin, " (", led, ") ON");
+	// removing unused pins (that existed in state and now do not)
+	for(let j=21; j<=23; ++j) {
+		
+		let state_pin = state.pins[j];
+		let found = 0;
+		
+		for(let i=0; i<obj.pins.length; ++i) {
+			let pin_number = obj.pins[i].number;
+			
+			if(pin_number === j) {
+				found = 1;
+				break;
 			}
-			else {
-				print("Setting pin ", leds[led].pin, " (", led, ") OFF");
-			}
-			GPIO.write(leds[led].pin, leds[led].state);
-		  }
-		  
-		  let publish_message = JSON.stringify({
-			  r: leds.r.state,
-			  g: leds.g.state,
-			  b: leds.b.state
-		  });
-		  
-		  print("Publishing state to ", state_topic, "topic with the following message:", publish_message);
-
-		  MQTT.pub(state_topic, publish_message, 1);
-	  }
-	  else {
-		 print("JSON parsing error: " + error);
-	  }
+		}
+		
+		if(!found) {
+			GPIO.write(j, 0);
+			print("Removing pin", j);
+		}
+	}
+	
+	state = new_state;
+	
+	let publish_message = JSON.stringify(state);
+	print("Publishing state to ", state_topic, "topic with the following message:", publish_message);
+	MQTT.pub(state_topic, publish_message, 1);
 
 }, null);
