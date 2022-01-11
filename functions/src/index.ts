7718bcdcf3ec7b61335671bafc305eb50d26fcfb
Felix Korovin <felix.korovin@icloud.com>
2022-01-11 18:27:26 +0200

    Cloud now handles correctly board deletion

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -8,24 +8,27 @@ admin.initializeApp();
 // On config change -> send to IoT core
 exports.configUpdate = functions.region(functions.config().iot.core.region).firestore.document("board-configs/{boardId}")
     .onWrite(
-        async (change: functions.Change<admin.firestore.DocumentSnapshot>, context?: functions.EventContext) => {
-          if (context) {
-            console.log(context.params.boardId);
+        async (change: functions.Change<admin.firestore.DocumentSnapshot>, context: functions.EventContext) => {
 
-            const data = change.after.data()
-            const config = data!.config
+          const boardId = context.params.boardId;
 
-            const formattedName = client.devicePath(process.env.GCLOUD_PROJECT!, functions.config().iot.core.region, functions.config().iot.core.registry, context.params.boardId);
-            const dataValue = Buffer.from(JSON.stringify(config)).toString("base64");
-
-            return client.modifyCloudToDeviceConfig({ 
-            	name: formattedName,
-            	binaryData: dataValue
-            });
-
-          } else {
-            throw (Error("no context from trigger"));
+          if(!change.after.exists) {
+            console.log(`The configuration of board ${boardId} has been deleted`);
+            return;
           }
+
+          console.log(`Sending a config update to board ${boardId}`);
+
+          const data = change.after.data()
+          const config = data!.config
+
+          const formattedName = client.devicePath(process.env.GCLOUD_PROJECT!, functions.config().iot.core.region, functions.config().iot.core.registry, boardId);
+          const dataValue = Buffer.from(JSON.stringify(config)).toString("base64");
+
+          return client.modifyCloudToDeviceConfig({
+          	name: formattedName,
+          	binaryData: dataValue
+          });
         }
     );
 
